ba5764eaa183d4acedb7fbc5c7728fac0dfd88f6
saddiqueakbaruku-a11y <saddiqueakbaruku@gmail.com>
2025-12-22 03:53:21 -0800

    changes in css file

--- 2-css/first.php
+++ 2-css/first.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <h1>fgdahakfdj</h1>
+    <h2>dhhds</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, necessitatibus veniam odit maxime omnis iure nobis hic deleniti vitae similique nisi reprehenderit blanditiis tenetur id eveniet! Commodi officiis vel quas?</p>
 </body>
 </html>
