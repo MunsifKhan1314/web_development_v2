75090496e40a010c30cd038134be4c891c8f1b04
MunsifKhan1314 <munsifkhan131413@gmail.com>
2025-12-22 01:48:53 -0800

    'changes'

--- 1-html/first.php
+++ 1-html/first.php
@@ -7,6 +7,30 @@
 </head>
 <body>
     <!-- headings -->
-     <h1></h1>
+     <h1>Hello</h1>
+     <!-- h1 h2 h3 h4 h5 h6 -->
+
+
+     <hr>
+<!-- Paragraph -->
+     <p>Paragraph</p>
+     <b>Bold</b>
+     <strong>Bold</strong>
+     <i>Italic</i>
+     <em>Italic</em>
+
+     <u>Underline</u>
+     <ins>Undersline</ins>
+     <!-- del sub sup small big  -->
+
+     <!-- list -->
+    <!-- 2 list type -->
+     <ol>
+        <li>Item</li>
+     </ol>
+
+     <ul>
+        <li>Item</li>
+     </ul>
 </body>
 </html>
