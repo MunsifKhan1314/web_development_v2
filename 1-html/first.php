3c0e7b7b76bba2386804716961432c2187ce157e
MunsifKhan1314 <munsifkhan131413@gmail.com>
2025-12-22 02:21:20 -0800

    changes

--- 1-html/first.php
+++ 1-html/first.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body >
+    <!-- style="height: 1100px; background-color: red;" -->
+    <p id="top"></p>
     <!-- headings -->
      <h1>Hello</h1>
      <!-- h1 h2 h3 h4 h5 h6 -->
@@ -25,12 +27,67 @@
 
      <!-- list -->
     <!-- 2 list type -->
-     <ol>
+     <ol start="100">
         <li>Item</li>
      </ol>
 
      <ul>
         <li>Item</li>
      </ul>
+    <!-- nested list -->
+     <ol>
+        <li> nested
+            <ul>
+                <li>Nested</li>
+            </ul>
+        </li>
+     </ol>
+     <!-- unorder list, type= sqaure, disc none circle, -->
+    <!-- order list start type= a A i I  -->
+     
+    <!-- table -->
+     <table border="1">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>F Name</th>
+                <th>Saving</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Munsif</td>
+                <td>Firdous Khan</td>
+                <td rowspan="2">Sad</td>
+            </tr>
+            <tr >
+                <!-- <td colspan="2">Sidiq</td> -->
+                 <td>:Lis</td>
+                 <td>hhds</td>
+                
+            </tr>
+            
+            
+        </tbody>
+        <!-- <tfoot>
+            <tr>
+                <th>Names</th>
+                <th>F Name</th>
+            </tr>
+        </tfoot> -->
+     </table>
+
+     <!-- colspan, colrow nested-->
+
+     <hr>
+<!-- links -->
+     <a href="">Link</a>
+    
+<img src="" width="300" alt="djf">
+<video controls width="200" height="200">
+    <source src="" type="video/mp4">
+</video>
+<a href="#top">Back to Top</a>
+
 </body>
 </html>
